a8abd059c6a3270efade6e7cba4b4fc783d94e5f
robi23-gunawan <60087536+robi23-gunawan@users.noreply.github.com>
2020-02-28 14:25:23 +0700

    new models/NewsLoad_Model.php

--- application/models/NewsLoad_Model.php
+++ application/models/NewsLoad_Model.php
@@ -1,7 +1,8 @@
 <?php 
     class NewsLoad_Model extends CI_Model {
         function __getdata(){
-            $this->db->limit(10);  // Produces: LIMIT 10
+            // $this->db->limit($limit);  // Produces: LIMIT 
+            $this->db->order_by("created_at", "desc"); //Produce: 
             return $this->db->get('berita');        
         }
     }
